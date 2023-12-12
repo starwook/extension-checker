1f50c420fa74e4e6b405da8f86f089d3d5ca8066
starwook <star.wookk@gmail.com>
2023-12-12 19:24:28 +0900

    refactor: Swagger 삭제

--- src/main/java/com/extension/checker/utility/ExtensionValidator.java
+++ src/main/java/com/extension/checker/utility/ExtensionValidator.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ExtensionValidator {
     public static final int MAX_LENGTH =20;
     public static final int MIN_LENGTH =1;
-    public static final int MAX_NOT_CUSTOM_EXTENSION_COUNT =200;
+    public static final int MAX_CUSTOM_EXTENSION_COUNT =200;
     public static boolean ifAddable(List<Extension> extensions,String newExtension){
         int customExtensionCount = 0;
         if(newExtension.length()> MAX_LENGTH || newExtension.length()<MIN_LENGTH) return false;
@@ -16,7 +16,7 @@ public class ExtensionValidator {
             if(extension.getName().equals(newExtension)) return false;
             if(extension.isCustom()) customExtensionCount++;
         }
-        if(customExtensionCount>MAX_NOT_CUSTOM_EXTENSION_COUNT) return false;
+        if(customExtensionCount> MAX_CUSTOM_EXTENSION_COUNT) return false;
         return true;
     }
 
