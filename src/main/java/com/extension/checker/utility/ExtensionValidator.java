7740a46e856cc2b3b6e61c777ae2a1404e1deaaf
starwook <star.wookk@gmail.com>
2023-12-12 19:18:36 +0900

    feat: 확장자 제한 조건 추가 (빈칸일 때 추가되지 않음)

--- src/main/java/com/extension/checker/utility/ExtensionValidator.java
+++ src/main/java/com/extension/checker/utility/ExtensionValidator.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class ExtensionValidator {
     public static final int MAX_LENGTH =20;
+    public static final int MIN_LENGTH =1;
     public static final int MAX_NOT_CUSTOM_EXTENSION_COUNT =200;
     public static boolean ifAddable(List<Extension> extensions,String newExtension){
         int customExtensionCount = 0;
-        if(newExtension.length()> MAX_LENGTH) return false;
+        if(newExtension.length()> MAX_LENGTH || newExtension.length()<MIN_LENGTH) return false;
         for(int i=0;i<extensions.size();i++){
             Extension extension = extensions.get(i);
             if(extension.getName().equals(newExtension)) return false;
